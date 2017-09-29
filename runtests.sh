c22f620805caabcdc8c50b214f7427dacbcac386
Kevin Luo <klevin.gluo@gmail.com>
2017-09-29 14:51:59 -0400

    made stageros headless

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,7 @@
 git --git-dir=/root/robotics/src/aau_multi_robot/.git pull origin master
 git --git-dir=/root/robotics/src/mappingturtles/.git pull origin master
 source /ros_entrypoint.sh;
+ln /dev/null /dev/raw1394
 catkin_make -C /root/robotics
 timelimit -t 180 roslaunch mappingturtles adhocbotstest.launch visualize:=false record:=true 
 
